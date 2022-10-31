789adaa2b6fae2b98dc848fd051da06ce454377c
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2022-10-31 21:58:18 +0700

    Just testing a litte bit...

--- ScoreMgmt/resource.h
+++ ScoreMgmt/resource.h
@@ -18,6 +18,8 @@
 #define ID_FILE_SAVEAS                  32774
 #define ID_EDIT_UNDO                    32775
 #define ID_EDIT_REDO                    32776
+#define ID_LANGUAGE_ENGLISH             32778
+#define ID_LANGUAGE_TI32779             32779
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -25,8 +27,8 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32780
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
